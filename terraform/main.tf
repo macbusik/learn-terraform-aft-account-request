6a7cbefb64acedd20f587923cf932f082b5c9b0a
Maciej Bus <macio@mac.home>
2025-11-09 23:17:47 +0100

    for goodnight

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,11 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "sandbox-1-aft-unique-aaa@example.com"
-    AccountName               = "sandbox-1-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "rafaltrzymalski@gmail.com"
-    SSOUserFirstName          = "Sandbox1"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-module "sandbox-2" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "sandbox-2-aft-unique-yyy@example.com"
-    AccountName               = "sandbox-2-aft"
+    AccountEmail              = "example-mail-for-sandbox123@example.com"
+    AccountName               = "sandboxTest1"
     ManagedOrganizationalUnit = "AFT Demo"
     SSOUserEmail              = "rafaltrzymalski@gmail.com"
-    SSOUserFirstName          = "Sandbox2"
+    SSOUserFirstName          = "Sandbox1"
     SSOUserLastName           = "AFT"
   }
 
